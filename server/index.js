b9b851d20aecb6a58985ed26298c47e6b235a62e
brygard14 <bryana.gardner@gmail.com>
2021-12-02 14:01:19 -0700

    set up server and rollbar

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
 
 const app = express()
 
+const { ROLLBAR_TOKEN } = process.env
+
 app.use(express.static('client'))
+
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.get('/', () => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4040
+
+rollbar.log('Hello World!')
 
 app.listen(port, () => console.log(`server running on port ${port}!`))
